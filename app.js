7a0ea30f2ceac18486b16f85f62d3ba8391167c3
kmsmk97 <smayantha97@gmail.com>
2025-02-03 16:38:47 +0530

    Database connected
    MongoDB Connected

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hehillo");
+//Pass = 5smyphone
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleware
+app.use("/",(req, res, next) => {
+    res.send("Database is working");
+}) 
+
+//Database connection
+
+mongoose.connect("mongodb+srv://admin:5smyphone@cluster0.bwq8h.mongodb.net/")
+.then(()=>console.log("Connected to MongoDB"))
+.then(()=>{
+    app.listen(5000);
+})
+
+.catch((err)=>console.log(err));
